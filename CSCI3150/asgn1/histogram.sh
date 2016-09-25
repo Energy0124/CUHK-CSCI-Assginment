f0fad844577f3bf4719a90194cafa9a29a469e84
Energy0124 <energystar.0124@gmail.com>
2016-09-26 05:32:39 +0800

    bug fix for case 3 of operation 1

--- CSCI3150/asgn1/histogram.sh
+++ CSCI3150/asgn1/histogram.sh
@@ -3,18 +3,24 @@
 op_type=${ASGN1_OP_TYPE}
 myfiles=${ASGN1_FILES}
 delimiters=${ASGN1_IFS}
+new_ifs=" "$'\n'$'\t'
+default_ifs=" "$'\n'$'\t'
 count=()
+IFS=${default_ifs}
 
 function histogram {
-
+    new_ifs=" "$'\n'$'\t'
+    for de in ${delimiters};do
+        new_ifs+=${de}
+    done
     for file in ${myfiles[@]};do
         if [ -f ${file} ]; then
             data=$(cat ${file})
+            IFS=${new_ifs}
             for str in ${data};do
-                IFS=${delimiters}
                 ((count[${#str}]++))
-                IFS=" "$'\n'$'\t'
             done
+            IFS=${default_ifs}
         else
             echo "File-error: ${file}!"
         fi
